cb821cce6e8da66152c02f163586ccf408cf5e64
KnogleMau <mav.solver@hotmail.com>
2025-09-20 12:57:08 +0200

    Gjort så vi henter alle film, men har problemer med og sorterer dem

--- src/main/java/app/services/MovieServices.java
+++ src/main/java/app/services/MovieServices.java
@@ -7,6 +7,7 @@ import app.entities.Movie;
 import app.entities.People;
 import jakarta.persistence.EntityManagerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,8 +16,8 @@ public class MovieServices {
     ApiServices apiServices = new ApiServices();
     String token = System.getenv("token");
 
-    public MovieResponseDTO getMoviesFromAPI(){
-        String uri = "https://api.themoviedb.org/3/discover/movie?language=da&page=1&region=DK&sort_by=popularity.desc&with_original_language=da&primary_release_date.gte=2020-09-15";
+    public MovieResponseDTO getMoviesFromAPI(int page){
+        String uri = "https://api.themoviedb.org/3/discover/movie?language=da&page="+page+"&region=DK&sort_by=popularity.desc&with_original_language=da&primary_release_date.gte=2020-09-15";
        return apiServices.fetchData(token, uri, MovieResponseDTO.class);
     }
 
@@ -24,16 +25,27 @@ public class MovieServices {
     public void getAllMoviesWithPeople(EntityManagerFactory emf) {
         PeopleServices ps = new PeopleServices();
         MovieDAO mDAO = new MovieDAO(emf);
-
-        MovieResponseDTO movieResponseDTO = getMoviesFromAPI();
-
         DTOToEntityConverter dtoToEntityConverter = new DTOToEntityConverter();
-        List<Movie> m = dtoToEntityConverter.convertMovieResponseToEntities(movieResponseDTO);
+        MovieResponseDTO movieResponseDTO = getMoviesFromAPI(1);
+
+        List<Movie> m = new ArrayList<>();
+        m.addAll(dtoToEntityConverter.convertMovieResponseToEntities(movieResponseDTO));
+
+        for(int i = 2; i <= movieResponseDTO.getTotal_pages(); i++){
+            movieResponseDTO = getMoviesFromAPI(i);
+            m.addAll(dtoToEntityConverter.convertMovieResponseToEntities(movieResponseDTO));
+        }
+
+
 
         for (Movie m1 : m) {
             int movieId = m1.getMovieId();
             PeopleResponseDTO peopleResponseDTO = ps.getPeopleFromDTO(movieId);
             List<People> p = dtoToEntityConverter.convertPeopleResponseToEntities(peopleResponseDTO);
+            p.forEach(System.out::println);
+
+
+            //List<People>FilterPeople = p.stream().filter(people -> "Actor".equalsIgnoreCase(people.getDepartment())|| "Director".equalsIgnoreCase(people.getDepartment())).toList();
             for (People people : p) {
                 m1.addPeople(people);
             }
